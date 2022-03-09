dce9da264c3cafa0f5a0f2bc34bb6ac22f38a1ac
Daniel Saraiva <danieldsvn@gmail.com>
2022-03-08 23:01:51 -0300

    Requisito 4 concluido

--- src/App.js
+++ src/App.js
@@ -3,12 +3,55 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+    };
+  }
+
+  onInputChange = ({ target }) => { // Ajuda do Matheus Vital
+    const { value, name, type, checked } = target;
+    this.setState({
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  }
+
   render() {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
+      cardImage, cardRare, cardTrunfo } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <p> arroz </p>
+        <Form
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.onInputChange }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -11,6 +11,7 @@ class Form extends React.Component {
         <label htmlFor="card-name">
           Nome da carta
           <input
+            name="cardName"
             type="text"
             data-testid="name-input"
             id="card-name"
@@ -21,6 +22,7 @@ class Form extends React.Component {
         <label htmlFor="description">
           Descrição
           <input
+            name="cardDescription"
             type="textarea"
             data-testid="description-input"
             id="description"
@@ -31,6 +33,7 @@ class Form extends React.Component {
         <label htmlFor="card-attr1">
           Atributo 1
           <input
+            name="cardAttr1"
             type="number"
             data-testid="attr1-input"
             id="card-attr1"
@@ -41,6 +44,7 @@ class Form extends React.Component {
         <label htmlFor="card-attr2">
           Atributo 2
           <input
+            name="cardAttr2"
             type="number"
             data-testid="attr2-input"
             id="card-attr2"
@@ -51,6 +55,7 @@ class Form extends React.Component {
         <label htmlFor="card-attr3">
           Atributo 3
           <input
+            name="cardAttr3"
             type="number"
             data-testid="attr3-input"
             id="card-attr3"
@@ -61,6 +66,7 @@ class Form extends React.Component {
         <label htmlFor="card-image">
           Link para imagem da carta
           <input
+            name="cardImage"
             type="text"
             data-testid="image-input"
             id="card-image"
@@ -71,6 +77,7 @@ class Form extends React.Component {
         <label htmlFor="how-rare">
           Raridade
           <select
+            name="cardRare"
             data-testid="rare-input"
             id="how-rare"
             value={ cardRare }
@@ -84,6 +91,7 @@ class Form extends React.Component {
         <label htmlFor="super-trunfo">
           É super Super Trunfo?
           <input
+            name="cardTrunfo"
             type="checkbox"
             data-testid="trunfo-input"
             id="super-trunfo"
